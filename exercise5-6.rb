9696e0fa740eab9ffcbbeeff900abbfeea2d2f69
jeffrothwell <rothwelljeff@gmail.com>
2017-12-11 18:03:17 -0500

    incorporate energy level meter

--- exercise5-6.rb
+++ exercise5-6.rb
@@ -1,4 +1,5 @@
 dist = 0
+energy = 10
 
 loop do
   puts "Would you like to walk, run or go home? (enter only \"walk\"/\"run\"/\"go home\" please)"
@@ -10,13 +11,20 @@ loop do
     mode = gets.chomp
   end
 
-  if mode == "walk"
-    dist += 1
-  elsif mode == "run"
-    dist += 5
+  while mode == "run" && energy < 3
+    puts "You're too tired to run right now, silly!\n Choose either walk or go home"
+    mode = gets.chomp
   end
 
-  puts "Distance from home is #{dist}"
+  if mode == "walk"
+    dist += 1
+    energy += 1
+  elsif mode == "run"
+    dist += 5
+    energy -= 3
+  end
+
+  puts "Distance from home is #{dist}\nEnergy level is #{energy}"
 
   break if mode == "go home"
 
