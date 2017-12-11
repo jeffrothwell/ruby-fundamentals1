727254c3b3c94932dc7d49ce308132cc018a4418
jeffrothwell <rothwelljeff@gmail.com>
2017-12-11 14:16:47 -0500

    finish exercise 3

---
+++ exercise3.rb
@@ -0,0 +1,9 @@
+puts "What is your name?"
+
+user_name = gets.chomp
+
+puts "Hello, #{user_name}, forgive me for being so forward, but how old are you?"
+
+user_age = gets.to_i
+
+puts "Ah, #{user_name} is a #{2017 - user_age} baby. Such a geezer"
